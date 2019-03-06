1f6efb308df8426d602359dcfda14d2ac909c88e
ebauer <ebauer@sohnix.local>
2019-03-06 16:31:26 +0100

    non-functional commit - placed `Graph` class in seperate file - `Graph`class now handles geomtry to image unit conversion - added `GraphProperties` class to handle geometry

--- cs/canvas-test/canvas-test/Form1.cs
+++ cs/canvas-test/canvas-test/Form1.cs
@@ -18,7 +18,7 @@ namespace canvas_test
         PictureBox surface;
         Bitmap canvas;
 
-        Graph g;
+        public Graph g;
         
         public Form1()
         {
@@ -42,7 +42,7 @@ namespace canvas_test
 
             canvas = new Bitmap(mainCon.Panel1.Width, mainCon.Panel1.Height);
 
-            g = new Graph(surface, canvas);
+            g = new Graph();
             //g.DrawCross();
             Point[] ps = new Point[5];
             ps[0] = new Point(10, 50);
@@ -75,166 +75,8 @@ namespace canvas_test
 
         public Graph GetGraph()
         {
-            return g;
+            return this.g;
         }
         
-    }
-
-    public class Graph
-    {
-        public static int DEFAULT_WIDTH = 100;
-        public static int DEFAULT_HEIGHT = 100;
-
-        private Color control;
-
-        private PictureBox imageBox;
-        private Bitmap image;
-        private Label labelX;
-        private Label labelY;
-        private bool transparent;
-
-        public Graph() : this(new PictureBox())
-        {
-
-        }
-
-        public Graph(PictureBox p) : this(p, new Bitmap(DEFAULT_WIDTH, DEFAULT_HEIGHT))
-        {
-
-        }
-
-        public Graph(PictureBox p, Bitmap b)
-        {
-            this.imageBox = p;
-            this.image = b;
-            this.imageBox.Image = this.image;
-
-            this.labelX = new Label
-            {
-                BorderStyle = BorderStyle.FixedSingle,
-                Text = "X Achse",
-                Parent = this.imageBox,
-                Top = 10,
-                Left = 10
-            };
-
-            this.labelY = new Label
-            {
-                BorderStyle = BorderStyle.FixedSingle,
-                Text = "Y Achse",
-                Parent = this.imageBox
-            };
-            this.labelY.Top = this.imageBox.Height - this.labelY.Height - 20;
-            this.labelY.Left = this.imageBox.Width - this.labelY.Width - 20;
-
-            this.control = this.labelX.BackColor;
-            this.transparent = false;
-
-        }
-
-        public int GetWidth()
-        {
-            return this.image.Width;
-        }
-
-        public int GetHeight()
-        {
-            return this.image.Height;
-        }
-
-        public void DrawCross()
-        {
-            Bitmap bmp = this.image;
-            int xm = bmp.Width;
-            int ym = bmp.Height;
-            this.DrawLine(new Point(0, ym), new Point(xm, 0));
-            this.DrawLine(new Point(0, 0), new Point(xm, ym));
-            /*
-            for (int x = 0; x < xm; x += 1)
-            {
-                for (int y = 0; y < ym; y += 1)
-                {
-                    if (y - x == 0 || y + x == xm)
-                    {
-                        bmp.SetPixel(x, y, Color.Black);
-                    }
-                }
-            }/* */
-        }
-
-        public void DrawLine(Point start, Point end)
-        {
-            // left and right points rather than start and end
-            Point Left = start.X < end.X ? start : end;
-            Point Right = start.X > end.X ? start : end;
-            // xdiff is always positive or zero
-            double xdiff = Right.X - Left.X;
-            // ydiff is positive if up-slope; negative if down-slope; zero if even line
-            double ydiff = Right.Y - Left.Y;
-            // steepness of the slope
-            double m = ydiff / xdiff;
-            m = Math.Truncate(m * 10000) / 10000;
-            double buffer = 0;
-            // remember y to count it up/down through x loops
-            int y = Left.Y;
-            for(int x=Left.X; x < Right.X; x+=1)
-            {
-                buffer += m;
-                if(Math.Abs(buffer) > 1)
-                {
-                    for(int yc=1; yc<Math.Abs(buffer); yc+=1)
-                    {
-                        this.SetPixel(x, y+(yc*Math.Sign(m)), Color.Black);
-                    }
-                    y += (int)Math.Truncate(buffer);
-                    buffer = Math.Truncate((buffer % 1.0) * 10000) / 10000;
-                } else
-                {
-                    this.SetPixel(x, y, Color.Black);
-                }
-            }
-        }
-
-        public void DrawLines(Point[] points, bool connect_ends=false)
-        {
-            if(points.Length >= 2)
-            {
-                Point current = points.First();
-                for(int pc=1; pc < points.Length; pc+=1)
-                {
-                    Point next = points.ElementAt(pc);
-                    this.DrawLine(current, next);
-                    current = next;
-                }
-                if(connect_ends)
-                {
-                    this.DrawLine(points.Last(), points.First());
-                }
-            }
-        }
-
-        public void SetPixel(int x, int y, Color fill)
-        {
-            this.image.SetPixel(x, y, fill);
-        }
-
-        public void SetLabelTransparency(bool transparent)
-        {
-            if(transparent)
-            {
-                this.labelX.BackColor = Color.Transparent;
-                this.labelY.BackColor = Color.Transparent;
-            } else
-            {
-                this.labelX.BackColor = this.control;
-                this.labelY.BackColor = this.control;
-            }
-        }
-
-        public void ToggleLabelTransparency()
-        {
-            this.transparent = !this.transparent;
-            this.SetLabelTransparency(this.transparent);
-        }
     }
 }
---
+++ cs/canvas-test/canvas-test/Graph.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Text;
+using System.Threading.Tasks;
+
+using ImageCoord = System.Drawing.Point;
+using GraphCoord = System.Drawing.PointF;
+
+
+namespace canvas_test
+{
+    /// <summary>
+    /// Repräsentiert die visuelle Schnittstelle um Graphen zu zeichnen
+    /// <para>Beinhaltet sowohl Bild-/ als auch Geometrieobjekte</para>
+    /// </summary>
+    public class Graph
+    {
+
+        #region public instance fields
+
+        public static int DEFAULT_WIDTH = 100;
+        public static int DEFAULT_HEIGHT = 100;
+
+        public Label LblY { get; set; }
+        public Label LblX { get; set; }
+        public Label[] LblLegende { get; set; }
+        public bool TransparentLabels { get; set; }
+
+        public Bitmap DrawArea { get; set; }
+        public PictureBox DrawAreaContainer { get; set; }
+        private GraphProperties Geometry { get; set; }
+
+        public Color[] VisualColors { get; set; }
+        public Color ForegroundColor { get; set; }
+        public Color BackgroundColor { get; set; }
+
+        #endregion
+
+        #region private instance fields
+
+        private int ImageWidth => DrawArea.Width;
+        private int ImageHeight => DrawArea.Height;
+
+        #endregion
+
+        #region constructors
+
+        public Graph()
+        {
+            DrawAreaContainer = new PictureBox();
+            DrawArea = new Bitmap(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+            DrawAreaContainer.Image = this.DrawArea;
+
+            LblX = new Label
+            {
+                BorderStyle = BorderStyle.FixedSingle,
+                Text = "X Achse",
+                Parent = DrawAreaContainer
+            };
+
+            LblY = new Label
+            {
+                BorderStyle = BorderStyle.FixedSingle,
+                Text = "Y Achse",
+                Parent = DrawAreaContainer
+            };
+
+            //WIP labelpositioning
+            LblX.Top = 10;
+            LblX.Left = 10;
+            LblY.Top = DrawAreaContainer.Height - LblY.Height - 20;
+            LblY.Left = DrawAreaContainer.Width - LblY.Width - 20;
+
+            BackgroundColor = LblX.BackColor;
+            TransparentLabels = false;
+
+            Geometry = new GraphProperties();
+        }
+
+        #endregion
+
+        #region public instance methods
+
+        /// <summary>
+        /// Beschriftungshintergründe durchsichtig/undurchsichtig umschalten
+        /// </summary>
+        public void ToggleLabelTransparency()
+        {
+            TransparentLabels = !TransparentLabels;
+            SetLabelTransparency(TransparentLabels);
+        }
+
+        /// <summary>
+        /// Zeichne eine Linie in den Graphen
+        /// </summary>
+        /// <param name="start">Start-Koordinate der Linie</param>
+        /// <param name="end">End-Koordinate der Linie</param>
+        public void DrawLine(Point start, Point end)
+        {
+            // left and right points rather than start and end
+            Point Left = start.X < end.X ? start : end;
+            Point Right = start.X > end.X ? start : end;
+            // xdiff is always positive or zero
+            float xdiff = Right.X - Left.X;
+            // ydiff is positive if up-slope; negative if down-slope; zero if even line
+            float ydiff = Right.Y - Left.Y;
+            // steepness of the slope
+            float m = ydiff / xdiff;
+            m = (float)Math.Truncate(m * 10000) / 10000;
+            float buffer = 0;
+            // remember y to count it up/down through x loops
+            int y = Left.Y;
+            for (int x = Left.X; x < Right.X; x += 1)
+            {
+                buffer += m;
+                if (Math.Abs(buffer) > 1)
+                {
+                    for (int yc = 1; yc < Math.Abs(buffer); yc += 1)
+                    {
+                        //TODO
+                        //this.SetPixel(x, y + (yc * Math.Sign(m)), Color.Black);
+                    }
+                    y += (int)Math.Truncate(buffer);
+                    buffer = (float)Math.Truncate((buffer % 1.0) * 10000) / 10000;
+                }
+                else
+                {
+                    //TODO
+                    //this.SetPixel(x, y, Color.Black);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Zeichne mehrere, verbundene Linien in den Graphen
+        /// </summary>
+        /// <param name="points">Liste der zu verbindenden Koordinaten</param>
+        /// <param name="connect_ends">Verbinde erste und letzte Koordinate</param>
+        public void DrawLines(Point[] points, bool connect_ends = false)
+        {
+            if (points.Length >= 2)
+            {
+                Point current = points.First();
+                for (int pc = 1; pc < points.Length; pc += 1)
+                {
+                    Point next = points.ElementAt(pc);
+                    DrawLine(current, next);
+                    current = next;
+                }
+                if (connect_ends)
+                {
+                    DrawLine(points.Last(), points.First());
+                }
+            }
+        }
+
+        public void DrawPoint(Point coord, int scale = 10)
+        {
+            //TODO
+        }
+
+        #endregion
+
+        #region private instance methods
+
+        private ImageCoord Graph2Image(GraphCoord gc)
+        {
+            GraphCoord relative = Geometry.GetRelative(gc);
+            // Die Asurichtung der X-Achse der Bitmap stimmt mit der X-Achse des Graphen überein
+            float relX = relative.X;
+            // Die Ausrichtung der Y-Achse der Bitmap entgegen der Y-Achse des Graphen
+            float relY = 1f - relative.Y;
+            int imgX = (int)Math.Floor((double)ImageWidth * relX);
+            int imgY = (int)Math.Floor((double)ImageHeight * relY);
+            return new ImageCoord(imgX, imgY);
+        }
+
+        private GraphCoord Image2Graph(ImageCoord ic)
+        {
+            return new GraphCoord(0, 0);
+        }
+
+        /// <summary>
+        /// Setze einen Pixel des Bitmap Objekts
+        /// </summary>
+        /// <param name="x">Horizontale Bild-Koordinate</param>
+        /// <param name="y">Vertikale Bild-Koordinate</param>
+        /// <param name="fill">Füllfarbe</param>
+        private void SetPixel(ImageCoord imageCoord, Color fill)
+        {
+            DrawArea.SetPixel(imageCoord.X, imageCoord.Y, fill);
+        }
+
+        private void SetPoint(GraphCoord gc, Color c) => SetPixel(Graph2Image(gc), c);
+
+        /// <summary>
+        /// Setze die Hintergrundfarbe der Label-Objekte
+        /// </summary>
+        /// <param name="transparent">T: Transparent; F: BackgroundColor</param>
+        private void SetLabelTransparency(bool transparent)
+        {
+            if (transparent)
+            {
+                LblX.BackColor = Color.Transparent;
+                LblY.BackColor = Color.Transparent;
+                foreach (Label lbl in LblLegende)
+                {
+                    lbl.BackColor = Color.Transparent;
+                }
+            }
+            else
+            {
+                LblX.BackColor = BackgroundColor;
+                LblY.BackColor = BackgroundColor;
+                foreach (Label lbl in LblLegende)
+                {
+                    lbl.BackColor = BackgroundColor;
+                }
+            }
+        }
+
+        #endregion
+
+    }
+
+    /// <summary>
+    /// Repräsentiert die Eigenschaften eines geometrischen Koordinatensystems
+    /// </summary>
+    class GraphProperties
+    {
+        #region public instance fields
+        public float LowX { get; set; }
+        public float HighX { get; set; }
+        public float LowY { get; set; }
+        public float HighY { get; set; }
+
+        /// <summary>
+        /// Grapheinteilung X-Achse
+        /// </summary>
+        public float ScalingX { get; set; }
+        /// <summary>
+        /// Grapheinteilung Y-Achse
+        /// </summary>
+        public float ScalingY { get; set; }
+
+        public float Width => HighX - LowX;
+        public float Height => HighY - LowY;
+        #endregion
+        #region private instance fields
+        #endregion
+
+        #region constructors
+
+        public GraphProperties()
+        {
+            LowX = 0.0f;
+            LowY = 0.0f;
+            HighX = 0.0f;
+            HighY = 0.0f;
+        }
+
+        #endregion
+
+        #region public instance methods
+
+        /// <summary>
+        /// Setzt neue low/high Grenzwerte der Y-Achse
+        /// <para>Reihenfolge der Werte hat keine Auswirkung</para>
+        /// </summary>
+        /// <param name="lim1">Erster Grenzwert</param>
+        /// <param name="lim2">Zweiter Grenzwert</param>
+        public void SetLimitsY(float lim1, float lim2)
+        {
+            if (lim1 <= lim2)
+            {
+                LowY = lim1;
+                HighY = lim2;
+            }
+            else
+            {
+                LowY = lim2;
+                HighY = lim1;
+            }
+        }
+
+        /// <summary>
+        /// Setzt neue low/high Grenzwerte der X-Achse
+        /// <para>Reihenfolge der Werte hat keine Auswirkung</para>
+        /// </summary>
+        /// <param name="lim1">Erster Grenzwert</param>
+        /// <param name="lim2">Zweiter Grenzwert</param>
+        public void SetLimitsX(float lim1, float lim2)
+        {
+            if (lim1 <= lim2)
+            {
+                LowX = lim1;
+                HighX = lim2;
+            }
+            else
+            {
+                LowX = lim2;
+                HighX = lim1;
+            }
+        }
+
+        /// <summary>
+        /// Wandelt absolute Koordinate in relative um
+        /// </summary>
+        /// <param name="absoluteCoord">Absolute Koordinate</param>
+        /// <returns>Relative Koordinate</returns>
+        public GraphCoord GetRelative(GraphCoord absoluteCoord)
+        {
+            // Division durch Null vermeiden
+            if (Width == 0.0)
+            {
+                return new GraphCoord(-1.0f, -1.0f);
+            }
+            return new GraphCoord(
+                (absoluteCoord.X - LowX) / Width,
+                (absoluteCoord.Y - LowY) / Height
+            );
+        }
+
+        #endregion
+        #region private instance methods
+        #endregion
+    }
+}
